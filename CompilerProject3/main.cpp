9fa12faa5b27806fb55230b9e6f6efe5a0d10928
PatrickDD <1229579784@qq.com>
2021-05-10 10:36:27 +0800

    Initial Commit

---
+++ CompilerProject3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CompilerProject3
+//
+//  Created by PatrickDD on 2021/5/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
